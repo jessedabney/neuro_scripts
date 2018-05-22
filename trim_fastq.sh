7256bcd0d60409b5f77fe635db540f13fca7c375
jessedabney <jesse.dabney@gmail.com>
2018-05-22 18:33:00 +0100

    fix bugs in trimming

--- trim_fastq.sh
+++ trim_fastq.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-for i in `sed 's/\.\///g'
+for i in `sed 's/\.\///g' ./samples.txt`
 do
-	trim_galore --fastqc_args '-t 8 -o ../../analysis/fastqc_out_trim' --retain_unpaired --paired ./${i}_1.fastq.gz ./${i}_2.fastq.gz
+  echo processing ${i}... >&2      
+  trim_galore --fastqc_args '-t 8 -o ../../analysis/fastqc_out_trim' --retain_unpaired --paired ./${i}_1.fastq.gz ./${i}_2.fastq.gz
+done
